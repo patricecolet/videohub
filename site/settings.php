57cd4114c663eb88e473c61196b53fabd6e0cb06
patrice colet <colet.patrice@free.fr>
2020-04-12 17:08:27 +0200

    Add files via upload

---
+++ site/settings.php
@@ -0,0 +1,19 @@
+<?php
+// ----------------------------------------------------------------------------
+/*
+PROJECT			:	megalegoland
+FILE				:	settings.php
+GOAL				:	Contains the main settings for this website
+AUTHOR			:	alien_genius
+COPYRIGHT		:	Copyright 2008 (c) Alien Genius Software Solutions
+*/
+// ----------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------
+define("ADMINUSERNAME","user");
+define("ADMINPASSWORD","login");
+// ----------------------------------------------------------------------------
+
+// MAIN ROUTINE
+// NO OUTPUT ON PURPOSE
+?>
