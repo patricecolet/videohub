4a8e55fc035ce70a9e548c3a56b085aa7c5c4ac5
patrice colet <colet.patrice@free.fr>
2020-04-18 14:34:31 +0200

    Delete readOGG.php

--- site/oggclass/readOGG.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("ogg.class.php");
-$video=new Ogg ("../video/blender/01.ogv");
-if ($video->LastError) { echo $video->LastError; exit; }
-$str = substr(strstr($video->Streams['summary'],"(theora): "),10);
-print_r(substr($str,0,strpos($str,"s")));
-?>
